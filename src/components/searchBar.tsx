1dcc649234895bc36443525211864cc1f814c342
SamuelCosta22 <samuelgcostainstitucional@gmail.com>
2025-03-20 11:42:00 -0300

    feat: add search bar

---
+++ src/components/searchBar.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { SearchIcon } from "lucide-react";
+import { Button } from "./ui/button";
+import { Input } from "./ui/input";
+import { useState } from "react";
+
+const SearchBar = () => {
+  const [search, setSearch] = useState("");
+  const handleSearch = () => {
+    console.log(search);
+    setSearch("");
+  };
+
+  return (
+    <div className="p-7 flex items-center gap-2">
+      <Input
+        placeholder="Pesquise por Flows..."
+        className="max-w-[282px] border-gray-300 bg-white px-4 py-2.5 text-sm outline-none"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      <Button className="text-white" onClick={handleSearch}>
+        <SearchIcon />
+      </Button>
+    </div>
+  );
+};
+
+export default SearchBar;
